8420a2eaf3fa97c6f948aa6133d6f4f6177488c6
vjraj19 <vijayraj5859@gmail.com>
2025-01-29 13:46:18 +0000

    changed greeting word

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome all!"
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return 'I am nice, how about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
