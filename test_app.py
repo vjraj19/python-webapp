5177ffeda14fa837f3a742c5eed47f36d345042f
vjraj19 <vijayraj5859@gmail.com>
2025-01-30 09:37:28 +0000

    Added unit tests

--- test_app.py
+++ test_app.py
@@ -38,5 +38,35 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/non-existent')
         self.assertEqual(response.status_code, 404)
 
+    def test_post_route(self):
+        response = self.app.post('/post', data={'name': 'John'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello John!')
+
+    def test_post_route_with_empty_name(self):
+        response = self.app.post('/post', data={'name': ''})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data.decode('utf-8'), 'Please provide a name.')
+
+    def test_post_route_with_invalid_name(self):
+        response = self.app.post('/post', data={'name': '123'})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data.decode('utf-8'), 'Please provide a valid name.')
+
+    def test_get_route_with_name(self):
+        response = self.app.get('/get?name=John')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello John!')
+
+    def test_get_route_with_empty_name(self):
+        response = self.app.get('/get?name=')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data.decode('utf-8'), 'Please provide a name.')
+
+    def test_get_route_with_invalid_name(self):
+        response = self.app.get('/get?name=123')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data.decode('utf-8'), 'Please provide a valid name.')
+
 if __name__ == '__main__':
     unittest.main()
